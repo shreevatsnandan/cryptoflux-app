af5ba04bbedd0ab5bfbc5f3b424ecf3e0830939a
shreevatsnandan <117607123+shreevatsnandan@users.noreply.github.com>
2024-04-26 03:03:34 +0530

    Update LazyLoadedArticles.js

--- src/Components/LazyLoadedArticles.js
+++ src/Components/LazyLoadedArticles.js
@@ -1,1 +1,18 @@
+import React from 'react';
 
+const LazyLoadedArticles = ({ articles }) => (
+    <div className="articles-container">
+        {articles.map((article, index) => (
+            <div key={index} className="article-card">
+                {article.urlToImage && <img src={article.urlToImage} alt={article.title} className="article-image" />}
+                <div className="article-details">
+                    <h3>{article.title}</h3>
+                    <p>{article.description}</p>
+                    <a href={article.url} target="_blank" rel="noopener noreferrer" className="read-more">Read More</a>
+                </div>
+            </div>
+        ))}
+    </div>
+);
+
+export default LazyLoadedArticles;
